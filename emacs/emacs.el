3f282ad5536bcaf9c6dddbcf6cf3ee22985607f6
Paul Grigoras <paul.grigoras90@gmail.com>
2014-01-15 00:13:16 +0000

    Tiny cleanups, highlight whitespace error.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,42 +1,28 @@
 ;; -- Useful built-ins --
 (require 'cl)
 
-;; -- package management -- (Based on
+;; -- Package management --
 (require 'package)
 (package-initialize)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-;; -- Fetch required packages --
-;; Based on a post by Boris Batsov
-;;  http://batsov.com/articles/2012/02/19/package-management-in-emacs-the-good-the-bad-and-the-ugly/)
 (defvar packages
   '(ack-and-a-half
     ;; coding stuff
-    ascope auto-complete yasnippet clojure-mode coffee-mode magit haskell-mode python inf-ruby
+    ascope auto-complete yasnippet magit haskell-mode python inf-ruby
     ;; writing
     ebib markdown-mode
     ;; ui
     expand-region paredit projectile popup
     volatile-highlights yaml-mode yari
     ;;themes
-    zenburn-theme ir-black-theme solarized-theme)
-  "A list of packages to ensure are installed at launch.")
+    zenburn-theme ir-black-theme solarized-theme))
 
-(defun packages-installed-p ()
-  (loop for p in packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
-
-
-(unless (packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  (dolist (p packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+(dolist (p packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 
 ;; -- imports --
@@ -48,36 +34,27 @@
 (require 'yasnippet)
 
 
-;; -- UI --
+;; -- UI, Editing --
+(desktop-save-mode 1)
 (global-hl-line-mode 0)
 (global-linum-mode 1)
+(ido-mode 1)
 (load-theme 'ir-black t)
 (menu-bar-mode -1)
 (setq column-number-mode t)
+(setq indent-tabs-mode nil)
+(setq tab-width 2)
+(setq whitespace-style '(face tabs trailing lines-tail))
 (show-paren-mode 1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-;;(volatile-highlights-mode t)
-(setq tab-width 2)
-(setq indent-tabs-mode nil)
-
-;; -- GDB configuration --
-(setq gdb-many-windows t)
-
-;; -- Enable ido switch buffer
-(ido-mode 1)
-
-;; -- Save open buffers on close --
-(desktop-save-mode 1)
+(volatile-highlights-mode t)
 
 ;; -- Coding --
 (define-key global-map (kbd "RET") 'newline-and-indent) ; Auto indent on enter
 (setq next-line-add-newlines t)                         ; Add new lines at end of buffer
-
-;; Setup yasnippet
+(setq gdb-many-windows t)
 (setq yas/root-directory '("~/.yasnippet-snippets"))
-
-;; Map `yas/load-directory' to every element
 (mapc 'yas/load-directory yas/root-directory)
 (yas-global-mode 1)
 (global-auto-complete-mode 1)
