40c70af542cefa0fd5096db9924b5fd23aa202d8
Paul Grigoras <paul.grigoras90@gmail.com>
2014-01-15 12:02:25 +0000

    Move backups to separate dir.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -73,6 +73,13 @@
 (yas-global-mode 1)
 (global-auto-complete-mode 1)
 
+;; -- Backups --
+(setq backup-directory-alist `(("." . "~/.emacs-bkp")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
 
 ;; -- Useful functions --
 (add-hook 'after-save-hook 'compile-latex-hook)
