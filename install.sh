a1c568d67ae1dd3dc022f565aada17369ee95cfa
Paul Grigoras <paul.grigoras90@gmail.com>
2014-01-16 14:53:55 +0000

    Add a list of useful packages to install.

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-dirs=(~/.emacs.el ~/.emacs.d ~/.yasnippet-snippets)
 
 # Check for existing files/simlinks
+echo "Installing Emacs 24 setup..."
+dirs=(~/.emacs.el ~/.emacs.d ~/.yasnippet-snippets)
+installEmacsSetup=1
 for dir in "${dirs[@]}"; do
     path=$(readlink -f ${dir})
     if [ -a ${path} ]; then
-        echo "${path} found! Please remove this before continuing..."
-        exit 1
+        echo -e "\t${path} found! Skipping Emacs setup installation."
+        echo -e "\tPlease remove ${path} and re-run the script to update your setup."
+        installEmacsSetup=0
+        break
     fi
 done
 
-# Create symlinks
-ln -s $(readlink -f emacs/emacs.el) ~/.emacs.el
-ln -s $(readlink -f emacs/yasnippet-snippets) ~/.yasnippet-snippets
+if [ $installEmacsSetup -eq 1 ]; then
+    echo "Creating symlinks."
+    ln -s $(readlink -f emacs/emacs.el) ~/.emacs.el
+    ln -s $(readlink -f emacs/yasnippet-snippets) ~/.yasnippet-snippets
+fi
+
+
+echo "Installing some useful packages..."
+packages=(maven python)
+
+for p in "${packages[@]}"; do
+    installed=$(dpkg -s "$p" 2> /dev/null)
+    if [ $? -eq 1 ]; then
+        echo -e "\t'$p' package not found! Installing..."
+        apt-get install $p -y
+    else
+        echo -e "\t[OK] '$p' package found"
+    fi
+done
