985cc41ec9dd15dd8455d8489cfef20afc8af285
Paul Grigoras <paul.grigoras90@gmail.com>
2014-02-15 14:32:22 +0000

    Delete trailing whitespace on save. Some minor color changes.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -97,6 +97,8 @@
       kept-new-versions 6
       kept-old-versions 2
       version-control t)
+;; -- Hooks --
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; -- Useful functions --
 (add-hook 'after-save-hook 'compile-latex-hook)
@@ -198,12 +200,18 @@
 
 (key-chord-define-global "qq" 'query-replace)
 
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "gray11" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(flycheck-error ((t (:foreground "firebrick"))))
+ '(flyspell-duplicate ((t (:foreground "Gold3" :weight bold))))
+ '(flyspell-duplicate-face ((t (:foreground "Gold3" :weight bold))) t)
+ '(flyspell-incorrect ((t (:foreground "firebrick4" :weight bold))))
+ '(flyspell-incorrect-face ((t (:foreground "firebrick4" :underline t :weight bold))) t)
  '(fringe ((t (:background "gray11"))))
  '(hl-line ((t (:color nil :style nil :background "#151515" :underline nil))))
  '(linum ((t (:inherit (shadow default) :background "gray11
