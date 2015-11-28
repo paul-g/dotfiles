26d0f5789e89c8eab2792d0565ca988053164209
Paul Grigoras <paul.grigoras90@gmail.com>
2015-11-28 12:57:58 +0000

    Clean up, add force flag and colours

--- install.sh
+++ install.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
+# Prompt Colours
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
 function fail() {
-    echo -e "    [FAIL] $1"
+    printf "${RED}    [FAIL]${NC} $1\n"
 }
 
 function skip() {
-    echo -e "    [SKIP] $1"
+    printf "${ORANGE}    [SKIP]${NC} $1\n"
 }
 
 function ok() {
-    echo -e "    [OK]  $1"
+    printf "${GREEN}    [OK]${NC}  $1\n"
 }
 
 function skipFoundFile() {
-    skip "$1 found! Skipping installation"
-    skip "    Please remove $1 to update your setup."
+    skip "$1 found! Use '-f' to overwrite"
+}
+
+function install() {
+  file=$1
+  flag=$2
+  dotfile=~/.${file}
+  configfile=$(readlink -f "config/${file}")
+
+  printf "${CYAN}Installing ${dotfile}${NC}\n"
+
+  if [ "${flag}" == "-f" ]; then
+    rm -f "${dotfile}"
+  fi
+
+  if [ -a $(readlink -f ${dotfile}) ]; then
+    skipFoundFile "${dotfile}"
+    return
+  fi
+
+  ln -s ${configfile} ${dotfile}
+  path=$(file ${dotfile})
+
+  ok "${path}"
 }
 
 # Installing some useful packages
@@ -30,23 +59,23 @@ packages=(alien ack-grep clang exuberant-ctags emacs24
           ipython-notebook python-pandas python-sympy python-nose)
 
 for p in "${packages[@]}"; do
-    installed=$(dpkg -s "$p" 2> /dev/null)
-    if [ $? -eq 1 ]; then
-        echo -e "\t'$p' package not found! Installing..."
-        apt-get install $p -y
-    else
-        ok "$p package found"
-    fi
+  installed=$(dpkg -s "$p" 2> /dev/null)
+  if [ $? -eq 1 ]; then
+      echo -e "\t'$p' package not found! Installing..."
+      apt-get install $p -y
+  else
+      ok "$p package found"
+  fi
 done
 
-# Installing some python packages
+ Installing some python packages
 echo "\nInstalling python packages..."
 packages=(pylint scipy elpy rope jedi epc)
 for p in "${packages[@]}"; do
     pip install "$p"
 done
 
-# Check for existing files/simlinks
+ Check for existing files/simlinks
 echo -e "\nInstalling Emacs 24 setup..."
 dirs=(~/.emacs.el ~/.emacs.d ~/.yasnippet-snippets)
 installEmacsSetup=1
@@ -65,39 +94,9 @@ if [ $installEmacsSetup -eq 1 ]; then
     ln -s $(readlink -f lib/prelude) ~/.emacs.d
 fi
 
+flag=$1
 
-# Install xmonad
-echo -e "\nInstalling xmonad..."
-if [ -a $(readlink -f ~/.xmonad) ]; then
-    skipFoundFile "~/.xmonad"
-else
-    ln -s $(readlink -f xmonad) ~/.xmonad
-    ln -s $(readlink -f xmonad/bin/xmonad.start) /usr/bin/xmonad.start
-fi
-
-
-# Install Git settings
-echo -e "\nInstalling git config..."
-if [ -a $(readlink -f ~/.gitconfig) ]; then
-    skipFoundFile "~/.gitconfig"
-else
-    ln -s $(readlink -f config/gitconfig) ~/.gitconfig
-fi
-
-echo -e "\nInstalling tmux config..."
-if [ -a $(readlink -f ~/.tmux.conf) ]; then
-    skipFoundFile "~/.tmux.conf"
-else
-    ln -s $(readlink -f config/tmux.conf) ~/.tmux.conf
-fi
-
-echo -e "\nInstalling vim config..."
-if [ -a $(readlink -f ~/.vimrc) ]; then
-    skipFoundFile "~/.vimrc"
-else
-    ln -s $(readlink -f config/vimrc) ~/.vimrc
-fi
-
-
-echo -e "\nInstalling zsh..."
-sh lib/oh-my-zsh/tools/install.sh
+install "vimrc" "${flag}"
+install "tmux.conf" "${flag}"
+install "gitconfig" "${flag}"
+#install "xmonad" "${flag}"
