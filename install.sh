e058c4e3908a755641e1e06ee0334bcc56f4df8a
Paul Grigoras <paul.grigoras90@gmail.com>
2015-11-28 21:31:35 +0000

    Decent flag parsing, don't install pckgs by default, add default inputrc

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ function install() {
 
   printf "${CYAN}Installing ${dotfile}${NC}\n"
 
-  if [ "${flag}" == "-f" ]; then
+  if [ "${flag}" == "1" ]; then
     rm -f "${dotfile}"
   fi
 
@@ -46,57 +46,74 @@ function install() {
   ok "${path}"
 }
 
-# Installing some useful packages
-echo "Installing some useful packages..."
-packages=(alien ack-grep clang exuberant-ctags emacs24
-          fakeroot gnuplot htop maven python python-pip
-	  meld colordiff inkscape zsh
-          pandoc subversion silversearcher-ag-el silversearcher-ag
-          xmonad xmobar libghc-xmonad-contrib-dev
-          texlive-full libghc-xmonad-dev suckless-tools trayer
-          valgrind terminator chromium-browser xscreensaver
-          python-numpy python-scipy python-matplotlib ipython
-          ipython-notebook python-pandas python-sympy python-nose)
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-for p in "${packages[@]}"; do
-  installed=$(dpkg -s "$p" 2> /dev/null)
-  if [ $? -eq 1 ]; then
-      echo -e "\t'$p' package not found! Installing..."
-      apt-get install $p -y
-  else
-      ok "$p package found"
-  fi
+install_packages=0
+force_install=0
+
+while getopts "hfp" opt; do
+    case "$opt" in
+    p)  install_packages=1
+        ;;
+    f)  force_install=1
+        ;;
+    esac
 done
 
- Installing some python packages
-echo "\nInstalling python packages..."
-packages=(pylint scipy elpy rope jedi epc)
-for p in "${packages[@]}"; do
-    pip install "$p"
-done
+if [ "${install_packages}" == "1" ]
+then
+  # Installing some useful packages
+  echo "Installing some useful packages..."
+  packages=(alien ack-grep clang exuberant-ctags emacs24
+            fakeroot gnuplot htop maven python python-pip
+      meld colordiff inkscape zsh
+            pandoc subversion silversearcher-ag-el silversearcher-ag
+            xmonad xmobar libghc-xmonad-contrib-dev
+            texlive-full libghc-xmonad-dev suckless-tools trayer
+            valgrind terminator chromium-browser xscreensaver
+            python-numpy python-scipy python-matplotlib ipython
+            ipython-notebook python-pandas python-sympy python-nose)
 
- Check for existing files/simlinks
-echo -e "\nInstalling Emacs 24 setup..."
-dirs=(~/.emacs.el ~/.emacs.d ~/.yasnippet-snippets)
-installEmacsSetup=1
-for dir in "${dirs[@]}"; do
-    path=$(readlink -f ${dir})
-    if [ -a ${path} ]; then
-        fail "${path} found! Skipping Emacs setup installation."
-        fail "Please remove ${path} and re-run the script to update your setup."
-        installEmacsSetup=0
-        break
+  for p in "${packages[@]}"; do
+    installed=$(dpkg -s "$p" 2> /dev/null)
+    if [ $? -eq 1 ]; then
+        echo -e "\t'$p' package not found! Installing..."
+        apt-get install $p -y
+    else
+        ok "$p package found"
     fi
-done
+  done
 
-if [ $installEmacsSetup -eq 1 ]; then
-    echo "Creating symlinks."
-    ln -s $(readlink -f lib/prelude) ~/.emacs.d
+  # Installing some python packages
+  echo "\nInstalling python packages..."
+  packages=(pylint scipy elpy rope jedi epc)
+  for p in "${packages[@]}"; do
+      pip install "$p"
+  done
+
+  # Check for existing files/simlinks
+  echo -e "\nInstalling Emacs 24 setup..."
+  dirs=(~/.emacs.el ~/.emacs.d ~/.yasnippet-snippets)
+  installEmacsSetup=1
+  for dir in "${dirs[@]}"; do
+      path=$(readlink -f ${dir})
+      if [ -a ${path} ]; then
+          fail "${path} found! Skipping Emacs setup installation."
+          fail "Please remove ${path} and re-run the script to update your setup."
+          installEmacsSetup=0
+          break
+      fi
+  done
+
+  if [ $installEmacsSetup -eq 1 ]; then
+      echo "Creating symlinks."
+      ln -s $(readlink -f lib/prelude) ~/.emacs.d
+  fi
 fi
 
-flag=$1
-
-install "vimrc" "${flag}"
-install "tmux.conf" "${flag}"
-install "gitconfig" "${flag}"
-install "bashrc" "${flag}"
+install "vimrc" "${force_install}"
+install "tmux.conf" "${force_install}"
+install "gitconfig" "${force_install}"
+install "bashrc" "${force_install}"
+install "inputrc" "${force_install}"
